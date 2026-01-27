9e2611ad741a9d830c76e0ab62e159527db7f68c
blackout0346 <149482684+blackout0346@users.noreply.github.com>
2026-01-27 13:32:42 +0800

    Add files via upload

--- server/Route.cpp
+++ server/Route.cpp
@@ -14,12 +14,16 @@ void Route::GET()
         cout << req.body << endl;
         return 200;
     });
-    CROW_ROUTE(app, "/Chat").methods("GET"_method)([](const crow::request req)
+    CROW_ROUTE(app, "/Chat").methods("GET"_method)([=](const crow::request req)
     {
             crow::json::rvalue x = crow::json::load(req.body);
 
             cout << x["ChatName"] << endl;
             cout << x["username"] << endl;
+            ostringstream  query;
+            query << "SELECT * FROM Chat";
+            db.SelectDatabase(query.str());
+            
 
             return 200;
     });
@@ -29,9 +33,8 @@ void Route::GET()
 
             cout << x["name"] << endl;
             cout << x["message"] << endl;
-
             ostringstream  query;
-            query << "SELECT M.Id, M.UserId, M.SendDate, M.ChatId, M.Msg, M.ReplyId FROM Message AS M JOIN User AS U ON U.id = M.UserId JOIN Chat AS C ON C.id = M.ChatId";
+            query << "SELECT M.Id, M.UserId, M.SendDate, M.ChatId, M.MsgId, M.ReplyId FROM MessageGET AS M JOIN User AS U ON U.id = M.UserId JOIN Chat AS C ON C.id = M.ChatId";
             db.SelectDatabase(query.str());
             return 200;
     });
@@ -71,16 +74,22 @@ void Route::POST()
         cout << x["name"] << endl;
         cout << x["password"] << endl;
         ostringstream  query;
-        query << "SELECT * FROM User WHERE name = " << x["name"].s() << "AND" << "password = " << x["password"] << ";";
+        query << "SELECT * FROM User WHERE name = " << x["name"].s() << "AND password = " << x["password"] << " OR NumberPhone = " << x["NumberPhone"]<< ";";
         db.DeleteDatabase(query.str());
         cout << " I got login!" << endl;
         return 200;
         });
-    CROW_ROUTE(app, "/CreateChat").methods("POST"_method)([](const crow::request& req) {
+    CROW_ROUTE(app, "/CreateChat").methods("POST"_method)([=](const crow::request& req) {
         crow::json::rvalue x = crow::json::load(req.body);
         cout << x["name"] << endl;
         cout << x["users"] << endl;
         cout << " I got chat!" << endl;
+        ostringstream  query;
+        ostringstream  query2;
+        query2 << "SELECT * FROM Roles as R WHERE name=" << x["users"] <<"JOIN User AS U ON U.id =R.UserId  " << ";";
+        query << "INSERT INTO Chat(Id,UserId1, UserId2,typeId) VALUES(" <<");";
+        db.AddinDatabase(query.str());
+        db.SelectDatabase(query2.str());
         return 200;
         });
     CROW_ROUTE(app, "/message").methods("POST"_method)([=](const crow::request req)
@@ -90,7 +99,7 @@ void Route::POST()
             cout << x["name"] << endl;
             cout << x["message"] << endl;
             ostringstream  query;
-            query << "INSERT INTO Message( Msg)VALUES(" << x["message"] << ")";
+            query << "INSERT INTO MessageSET(Msg)VALUES(" << x["message"] << ")";
             db.AddinDatabase(query.str());
             return 200;
         });
@@ -98,24 +107,26 @@ void Route::POST()
 }
 void Route::DELETEV()
 {
-    CROW_ROUTE(app, "/Users").methods("DELETE"_method)([=](const crow::request req)
+    CROW_ROUTE(app, "/users/delete").methods("DELETE"_method)([=](const crow::request req)
         {
             crow::json::rvalue x = crow::json::load(req.body);
-
             cout << x["name"] << endl;
-            cout << x["password"] << endl;
             ostringstream  query;
             query << "DELETE FROM User WHERE id = " << x["name"].s() << ";";
             db.DeleteDatabase(query.str());
-            cout << req.body << endl;
             return 200;
         });
     CROW_ROUTE(app, "/message/delete").methods("DELETE"_method)([=](const crow::request req)
         {
-            ostringstream  query;
+            ostringstream query;
+            ostringstream query2;
             crow::json::rvalue x = crow::json::load(req.body);
             cout << x["message"] << endl;
-            query << "DELETE FROM MessageSET Where ";
+            
+            query << "DELETE FROM MessageSET AS M WHERE M.Id = " << x["message"] <<";";
+            query2 << "DELETE FROM MessageGET AS M WHERE M.Id = ";
+            db.DeleteDatabase(query.str());
+            db.DeleteDatabase(query2.str());
             return 200;
         });
     PORT();
